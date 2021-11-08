adde19290903d6ba557854dbedd84110ce5f05f1
THAMIRES GAUDENCIO AMÂNCIO <20272597@cfp127aluno.local>
2021-11-08 11:23:19 -0300

    08/11 chat funcionando

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const socket = require("socket.io");
+const app = express();
+const cors = require("cors");
+const http = require ("http")
+const { Server } = require("socket.io")
+
+app.use(cors());
+app.use(express.json());
+
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+
+
+io.on("connection", (socket) => {
+  console.log(`User connected at : ${socket.id}`);
+
+  socket.on("join_room", (data, name) => {
+  
+    socket.join(data, name);
+    console.log(
+      `User with id : ${socket.id}, joined room : ${data} he name is : ${name}`
+    );
+  
+  });
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message",data)
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnect");
+  });
+});
+
+server.listen(3001, () => {
+  console.log("server is running");
+});
